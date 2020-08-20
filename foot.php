cb90ba0d1bb33d7f6d0fa537053690c16383d3e1
Rodrigo Corvalan <rcorvalan@armix.com.ar>
2020-08-20 16:10:08 -0300

    1

--- foot.php
+++ foot.php
@@ -1,6 +1,8 @@
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+<?php /*
 <script defer src="https://maps.googleapis.com/maps/api/js"></script>
 <script src="javascripts/locations.js"></script>
+*/ ?>
 <script src="javascripts/menu-float.js"></script>
 
 </body>
--- index.php
+++ index.php
@@ -5,8 +5,13 @@ require 'head.php';
 $show_default = true;
 require 'parts/organisms/menu-float.php';
 require 'parts/organisms/locations-bar.php';
+
+echo '<div class="main-content">';
+
 require 'parts/organisms/header.php';
 require 'parts/organisms/empty.php';
 require 'parts/organisms/footer.php';
+
+echo '</div>';
 
 require 'foot.php';
