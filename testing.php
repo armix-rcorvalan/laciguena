a44a1a0a14fa8ba993dfd940904dae62d7749bd1
Rodrigo Corvalan <rcorvalan@armix.com.ar>
2020-08-20 17:52:57 -0300

    1

---
+++ testing.php
@@ -0,0 +1,17 @@
+<?php
+
+require 'head.php';
+
+$show_default = true;
+require 'parts/organisms/menu-float.php';
+require 'parts/organisms/locations-bar.php';
+
+echo '<div class="main-content">';
+
+require 'parts/organisms/header.php';
+require 'parts/organisms/testing.php';
+require 'parts/organisms/footer.php';
+
+echo '</div>';
+
+require 'foot.php';
